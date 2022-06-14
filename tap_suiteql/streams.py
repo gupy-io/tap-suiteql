7651f8efcbf3ac7f7f9d17e4fefa57e5cb528d31
lorransr <lorran.rodr@gmail.com>
2022-06-14 15:38:13 -0300

    Replace concatenated strings to multi-line strings

--- tap_suiteql/streams.py
+++ tap_suiteql/streams.py
@@ -8,12 +8,12 @@ class SubscriptionStream(suiteqlStream):
     path = "/query/v1/suiteql"
     metadata_path = "/record/v1/metadata-catalog/subscription"
     # Always sort the replication key and format the replication_key
-    body_query = (
-        "select * "
-        ",TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') as lastmodifieddatetime "
-        "from subscription "
-        "order by TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') ASC"
-    )
+    body_query = """
+        select *
+        ,TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') as lastmodifieddatetime
+        from subscription
+        order by TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') ASC
+        """
     primary_keys = ["id"]
     replication_key = "lastmodifieddatetime"
 
@@ -23,12 +23,12 @@ class CustomerStream(suiteqlStream):
     path = "/query/v1/suiteql"
     metadata_path = "/record/v1/metadata-catalog/customer"
     # Always sort the replication key and format the replication_key
-    body_query = (
-        "select * "
-        ",TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') as lastmodifieddatetime "
-        "from customer "
-        "order by TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') ASC"
-    )
+    body_query = """
+        select *
+        ,TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') as lastmodifieddatetime
+        from customer
+        order by TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') ASC
+        """
     primary_keys = ["id"]
     replication_key = "lastmodifieddatetime"
 
@@ -38,12 +38,12 @@ class InvoiceStream(suiteqlStream):
     path = "/query/v1/suiteql"
     metadata_path = "/record/v1/metadata-catalog/invoice"
     # Always sort the replication key and format the replication_key
-    body_query = (
-        "select * "
-        ",TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') as lastmodifieddatetime "
-        "FROM transaction where type = 'CustInvc'"
-        "order by TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') ASC"
-    )
+    body_query = """
+        select *
+        ,TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') as lastmodifieddatetime
+        FROM transaction where type = 'CustInvc'
+        order by TO_CHAR(lastmodifieddate, 'YYYY-MM-DD HH:MI:SS') ASC
+        """
     primary_keys = ["id"]
     replication_key = "lastmodifieddatetime"
 
