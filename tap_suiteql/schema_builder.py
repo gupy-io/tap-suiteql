1e5d862f4216e65bd55b9bc2a01ad5edd017af45
lorransr <lorran.rodr@gmail.com>
2022-07-05 12:58:32 -0300

    set attribute type DateTimeType based on metadata

--- tap_suiteql/schema_builder.py
+++ tap_suiteql/schema_builder.py
@@ -7,17 +7,21 @@ class SchemaBuilder:
     def __init__(self, stream: suiteqlStream):
         self.stream: suiteqlStream = stream
 
-    def _schema_builder(self, attributes: list[str]) -> dict:
+    def _schema_builder(self, attributes: dict) -> dict:
         property_list = th.PropertiesList()
-
-        for attribute in attributes:
-            if attribute != "links":
-                property_list.append(th.Property(attribute.lower(), th.StringType))
-
         if self.stream.replication_key:
-            property_list.append(
-                th.Property(self.stream.replication_key, th.DateTimeType)
-            )
+            attributes.update({self.stream.replication_key: "date"})
+
+        for attribute_name, attribute_type in attributes.items():
+            if attribute_name != "links":
+                if attribute_type == "date":
+                    property_list.append(
+                        th.Property(attribute_name.lower(), th.DateTimeType)
+                    )
+                else:
+                    property_list.append(
+                        th.Property(attribute_name.lower(), th.StringType)
+                    )
 
         return property_list.to_dict()
 
@@ -25,6 +29,9 @@ class SchemaBuilder:
         if self.stream.schema["properties"] == {}:
             record = self.stream.get_metadata()
             attributes = record["x-ns-filterable"]
-            return self._schema_builder(attributes)
+            attributes_dict = {
+                a: record["properties"][a].get("format") for a in attributes
+            }
+            return self._schema_builder(attributes_dict)
         else:
             return self.stream.schema
--- tap_suiteql/tests/test_schema_builder.py
+++ tap_suiteql/tests/test_schema_builder.py
@@ -6,44 +6,53 @@ from tap_suiteql.schema_builder import SchemaBuilder
 class DummyStream:
     schema = {"type": "object", "properties": {}}
     replication_key = "somekey"
+
     def get_metadata(self):
         return {
+            "properties": {
+                "custbody_o2s_transaction_t_nr_seq_ad": {"format": None},
+                "custbody_o2s_transaction_d_contingenci": {"format": None},
+                "custbody_o2s_transaction_c_outra_reten": {"format": "date"},
+                "custbody_o2s_to_subsidiary_t_logradour": {"format": None},
+                "renewalNumber": {"format": "int64"},
+            },
             "x-ns-filterable": [
                 "custbody_o2s_transaction_t_nr_seq_ad",
                 "custbody_o2s_transaction_d_contingenci",
                 "custbody_o2s_transaction_c_outra_reten",
                 "custbody_o2s_to_subsidiary_t_logradour",
                 "renewalNumber",
-            ]
+            ],
         }
+
 
 class DummyStreamWithSchema:
     schema = th.PropertiesList(
-                th.Property("catalogtype", th.StringType),
-                th.Property("chargetype", th.StringType),
-                th.Property("enddate", th.StringType),
-                th.Property("frequency", th.StringType),
-                th.Property("id", th.StringType),
-                th.Property("item", th.StringType),
-                th.Property("linenumber", th.StringType),
-                th.Property("priceplan", th.StringType),
-                th.Property("prorateby", th.StringType),
-                th.Property("proratebyoption", th.StringType),
-                th.Property("quantity", th.StringType),
-                th.Property("recurringamount", th.StringType),
-                th.Property("repeatevery", th.StringType),
-                th.Property("startdate", th.StringType),
-                th.Property("startoffsetvalue", th.StringType),
-                th.Property("status", th.StringType),
-                th.Property("subscription", th.StringType),
-            ).to_dict()
+        th.Property("catalogtype", th.StringType),
+        th.Property("chargetype", th.StringType),
+        th.Property("enddate", th.StringType),
+        th.Property("frequency", th.StringType),
+        th.Property("id", th.StringType),
+        th.Property("item", th.StringType),
+        th.Property("linenumber", th.StringType),
+        th.Property("priceplan", th.StringType),
+        th.Property("prorateby", th.StringType),
+        th.Property("proratebyoption", th.StringType),
+        th.Property("quantity", th.StringType),
+        th.Property("recurringamount", th.StringType),
+        th.Property("repeatevery", th.StringType),
+        th.Property("startdate", th.StringType),
+        th.Property("startoffsetvalue", th.StringType),
+        th.Property("status", th.StringType),
+        th.Property("subscription", th.StringType),
+    ).to_dict()
 
 
 def test_schema_definition():
     expected = th.PropertiesList(
         th.Property("custbody_o2s_transaction_t_nr_seq_ad", th.StringType),
         th.Property("custbody_o2s_transaction_d_contingenci", th.StringType),
-        th.Property("custbody_o2s_transaction_c_outra_reten", th.StringType),
+        th.Property("custbody_o2s_transaction_c_outra_reten", th.DateTimeType),
         th.Property("custbody_o2s_to_subsidiary_t_logradour", th.StringType),
         th.Property("renewalnumber", th.StringType),
         th.Property("somekey", th.DateTimeType),
