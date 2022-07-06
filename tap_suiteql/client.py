48992fdfe363a380fc2f044d6c8c04bfcfcfb26b
lorransr <lorran.rodr@gmail.com>
2022-07-06 09:46:52 -0300

    transform get_column_select into a class method

--- tap_suiteql/client.py
+++ tap_suiteql/client.py
@@ -2,7 +2,7 @@
 
 import logging
 import re
-from typing import Any, Dict, Optional, cast
+from typing import Any, Dict, Optional, cast, List
 from urllib.parse import parse_qsl, urlparse
 
 import backoff
@@ -173,12 +173,8 @@ class QueryBuilder:
     def __init__(self, stream: suiteqlStream):
         self.stream: suiteqlStream = stream
 
-    def _query_builder(
-        self, schema: dict, replication_key: str, entity_name: str, stream_type: str
-    ) -> str:
-        from_statement = f"from {entity_name}"
+    def _get_column_select(self, schema: dict) -> List:
         column_select = []
-        where_clauses = ["1=1"]
         for attribute_name, properties in schema["properties"].items():
             if properties.get("format") == "date-time":
                 column_select.append(
@@ -186,15 +182,21 @@ class QueryBuilder:
                 )
             else:
                 column_select.append(attribute_name)
+        return column_select
+
+    def _query_builder(
+        self, schema: dict, replication_key: str, entity_name: str, stream_type: str
+    ) -> str:
+        from_statement = f"from {entity_name}"
+        where_clauses = ["1=1"]
+        column_select = self._get_column_select(schema)
         if replication_key:
             where_clauses.append(
                 f"{replication_key} >= TO_DATE(:{replication_key}, 'YYYY-MM-DD\"T\"HH24:MI:SS')"
             )
-
         if stream_type:
             from_statement = f"from transaction"
             where_clauses.append(f"type = '{stream_type}'")
-
         SELECT_STATEMENT += ",".join(column_select)
         WHERE_STATEMENT += " and ".join(where_clauses)
         query = f"{SELECT_STATEMENT} {from_statement} {WHERE_STATEMENT}".strip()
